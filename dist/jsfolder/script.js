ae39ac1d0694f76c969b8e6ffa703e3e9598c20d
AlcariaJoshua <alcariajoshua13@gmail.com>
2023-07-28 10:54:36 +0800

    sass fri

--- dist/jsfolder/script.js
+++ dist/jsfolder/script.js
@@ -22,3 +22,17 @@ window.addEventListener("scroll", () => {
         kamote.classList.remove("white");
     }
 });
+
+const modal = document.querySelector('.modal');
+const btnRegion = document.querySelector('.region');
+const btnClose = document.querySelector('#closeModal');
+
+btnRegion.addEventListener('click',()=>{
+    btnRegion.classList.toggle('active')
+    btnRegion.nextElementSibling.classList.toggle('active')
+})
+
+btnClose.addEventListener('click',()=>{
+    modal.classList.toggle('active');
+    btnRegion.classList.toggle('active')
+})
